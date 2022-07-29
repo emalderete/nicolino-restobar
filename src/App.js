a689aa9b9f2457120e8d053dc28f1f52fb839b3f
emalderete <nicolas.alderete95@live.com>
2022-07-29 16:30:36 -0300

    Se modifica el import de las hojas de estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import './App.css';
+import './Responsive.css';
 
 function App() {
   return (
