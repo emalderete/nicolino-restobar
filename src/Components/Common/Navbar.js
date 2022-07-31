2f71207e9d8252f4abf2d073cbc3e15a1d7690f2
emalderete <nicolas.alderete95@live.com>
2022-07-31 14:50:28 -0300

    Creado Navbar y mininavbar
    • Se crea Navbar para resoluciones superiores a 720 pixeles de ancho.
    • Se crea un mininavbar para resoluciones menores de 720 pixeles de ancho.
    • Creado botón para desplegar el mininavbar en moviles.
    • Modificado el idioma del sitio web de "en" a "es" en el index.html de la carpeta "public".
    • Modificaciones sobre la adaptabilidad de los elementos en diferentes resoluciones.
    • Reemplazada la imagen del logotipo del sitio web, se cambió la versión de formato ".svg" por ".png" dado que la primera presentaba problemas con la fuente de letra a mostrar.

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import brand from '../../img/ui/nicolino-restobar.png';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
@@ -7,14 +8,22 @@ const Navbar = () => {
             <div className='navbarContainer'>
                 <div className='navbar'>
                     <div className='brandContainer'>
-                        <span className='Brand'>Nicolino Restobar</span>
+                        <NavLink to='/'><img className='brand' src={brand} alt=''></img></NavLink>
                     </div>
                     <ul className='navbarItems'>
-                        <li className='navbarItem'><NavLink to='menu'>Menu</NavLink></li>
-                        <li className='navbarItem'><NavLink to='events'>Events</NavLink></li>
-                        <li className='navbarItem'><NavLink to='about'>About Us</NavLink></li>
+                        <li className='navbarItem'><NavLink className='navLink' to='menu'><i className='fa-solid fa-utensils'></i> Menu</NavLink></li>
+                        <li className='navbarItem'><NavLink className='navLink' to='events'><i className='fa-solid fa-calendar-check'></i> Events</NavLink></li>
+                        <li className='navbarItem'><NavLink className='navLink' to='about'><i className='fa-solid fa-users'></i> About Us</NavLink></li>
                     </ul>
+                    <button className='miniNavbarButton'><i className='fa-solid fa-angle-down'></i></button>
                 </div>
+            </div>
+            <div className='miniNavbar'>
+                <ul className='miniNavbarItems'>
+                    <li className='miniNavbarItem'><NavLink className='navLink miniNavLink' to='menu'><i className='fa-solid fa-utensils'></i> Menu</NavLink></li>
+                    <li className='miniNavbarItem'><NavLink className='navLink miniNavLink' to='events'><i className='fa-solid fa-calendar-check'></i> Events</NavLink></li>
+                    <li className='miniNavbarItem'><NavLink className='navLink miniNavLink' to='about'><i className='fa-solid fa-users'></i> About Us</NavLink></li>
+                </ul>
             </div>
         </div>
     );
