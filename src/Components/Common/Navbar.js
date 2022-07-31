ae8ba5918c67c94e0fa22b58a5c82201c7ddc10e
emalderete <nicolas.alderete95@live.com>
2022-07-31 15:12:57 -0300

    Agregada funcionalidad del botón y mininavbar
    • Se crea hook de estado para guardar el booleano que dará acción al mininavbar y la animación del botón disparador del mismo.

--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import brand from '../../img/ui/nicolino-restobar.png';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    const [miniNavbarActive, setMiniNavbarActive] = useState(false);
+
+    function navbarHandler(){
+        miniNavbarActive ? setMiniNavbarActive(false) : setMiniNavbarActive(true);
+    }
+
     return (
         <div>
             <div className='navbarContainer'>
@@ -15,10 +21,10 @@ const Navbar = () => {
                         <li className='navbarItem'><NavLink className='navLink' to='events'><i className='fa-solid fa-calendar-check'></i> Events</NavLink></li>
                         <li className='navbarItem'><NavLink className='navLink' to='about'><i className='fa-solid fa-users'></i> About Us</NavLink></li>
                     </ul>
-                    <button className='miniNavbarButton'><i className='fa-solid fa-angle-down'></i></button>
+                    <button style={miniNavbarActive ? {transform: 'rotateY(180deg)'} : null} className='miniNavbarButton' onClick={navbarHandler}><i className='fa-solid fa-angle-down'></i></button>
                 </div>
             </div>
-            <div className='miniNavbar'>
+            <div style={miniNavbarActive ? {transform : 'translateY(5rem)'} : {transform : 'translateY(-10rem)'}} className='miniNavbar'>
                 <ul className='miniNavbarItems'>
                     <li className='miniNavbarItem'><NavLink className='navLink miniNavLink' to='menu'><i className='fa-solid fa-utensils'></i> Menu</NavLink></li>
                     <li className='miniNavbarItem'><NavLink className='navLink miniNavLink' to='events'><i className='fa-solid fa-calendar-check'></i> Events</NavLink></li>
